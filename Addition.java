024f34507590244e604f159de7326100ac14b2b0
DivyaNeelam <divyaneelamneelam@gmail.com>
2025-07-18 17:15:57 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String [] args){
+        int sum=add(13,23);
+        System.out.println("sum of 13 and 23 is: "+sum);
+    }
+}

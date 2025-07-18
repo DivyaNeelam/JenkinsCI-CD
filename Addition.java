6916c144de47eb349bc6b48ee6df37d540bf48d8
DivyaNeelam <divyaneelamneelam@gmail.com>
2025-07-18 17:05:05 +0530

    Delete Addition.java

--- Addition.java
+++
@@ -1,9 +0,0 @@
-public class Addition{
-    public static void add(int a,int b){
-        return a+b;
-    }
-    public static void main(String [] args){
-        int sum=add(13,23);
-        System.out.println("sum of 13 and 23 is: "+sum);
-    }
-}
